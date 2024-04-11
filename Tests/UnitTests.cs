4fb4d2107044ddc2f6b479f605210803eda46b99
David Khuu <dkhuu2@huskers.unl.edu>
2024-04-11 18:34:53 -0500

    incorrect unit tests

--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -31,5 +31,84 @@ namespace GithubActionsLab
         }
 
         // Implement 3 tests per operation, following a similar pattern as above
+        [Test]
+        public void Subtract_ValidKhuu()
+        {
+            Assert.AreEqual(0, Program.Subtract("1", "1"));
+            Assert.AreEqual(7, Program.Subtract("6", "0"));
+            Assert.AreEqual(0, Program.Subtract("2", "2"));
+        }
+
+        [Test]
+        public void Subtract_InvalidKhuu()
+        {
+            Assert.Throws<FormatException>(() => Program.Subtract("1", "a"));
+            Assert.Throws<FormatException>(() => Program.Subtract("a", "1"));
+            Assert.Throws<FormatException>(() => Program.Subtract("a", "a"));
+        }
+
+        [Test]
+        public void Subtract_NullKhuu()
+        {
+            Assert.Throws<ArgumentNullException>(() => Program.Subtract("1", null));
+            Assert.Throws<ArgumentNullException>(() => Program.Subtract(null, "1"));
+            Assert.Throws<ArgumentNullException>(() => Program.Subtract(null, null));
+        }
+
+        [Test]
+        public void Multiply_ValidKhuu()
+        {
+            Assert.AreEqual(1, Program.Multiply("1", "1"));
+            Assert.AreEqual(0, Program.Multiply("9", "0"));
+            Assert.AreEqual(5, Program.Multiply("1", "5"));
+        }
+
+        [Test]
+        public void Multiply_InvalidKhuu()
+        {
+            Assert.Throws<FormatException>(() => Program.Multiply("1", "a"));
+            Assert.Throws<FormatException>(() => Program.Multiply("a", "1"));
+            Assert.Throws<FormatException>(() => Program.Multiply("a", "a"));
+        }
+
+        [Test]
+
+        public void Multiply_NullKhuu()
+
+        {
+            Assert.Throws<ArgumentNullException>(() => Program.Multiply("1", null));
+            Assert.Throws<ArgumentNullException>(() => Program.Multiply(null, "1"));
+            Assert.Throws<ArgumentNullException>(() => Program.Multiply(null, null));
+        }
+
+        [Test]
+
+        public void Divide_ValidKhuu()
+
+        {
+            Assert.AreEqual(6, Program.Divide("6", "1"));
+            Assert.AreEqual(5, Program.Divide("15", "3"));
+            Assert.AreEqual(10, Program.Divide("100", "10"));
+        }
+
+        [Test]
+
+        public void Divide_InvalidKhuu()
+
+        {
+            Assert.Throws<FormatException>(() => Program.Divide("1", "a"));
+            Assert.Throws<FormatException>(() => Program.Divide("a", "1"));
+            Assert.Throws<FormatException>(() => Program.Divide("a", "a"));
+        }
+
+        [Test]
+
+        public void Divide_NullKhuu()
+        {
+            Assert.Throws<ArgumentNullException>(() => Program.Divide("1", null));
+            Assert.Throws<ArgumentNullException>(() => Program.Divide(null, "1"));
+            Assert.Throws<ArgumentNullException>(() => Program.Divide(null, null));
+        }
+
     }
 }
